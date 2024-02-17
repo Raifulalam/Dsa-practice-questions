24256cbae52f7298954134b1831b3eef6ea79197
Raifulalam <raifulalam0123@gmail.com>
2024-02-17 22:01:58 +0530

    create node class

---
+++ src/main/java/node/Node.java
@@ -0,0 +1,14 @@
+package node;
+
+public class Node {
+    int data;
+    Node next;
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+    }
+}

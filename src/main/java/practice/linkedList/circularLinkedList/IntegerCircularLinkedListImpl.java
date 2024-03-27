e8b8d2d5f5fc7810b5b15f17fb642536421b882d
Raifulalam <raifulalam0123@gmail.com>
2024-03-28 01:57:22 +0530

    getSize and isEmpty method completed in circular linked list

--- src/main/java/practice/linkedList/circularLinkedList/IntegerCircularLinkedListImpl.java
+++ src/main/java/practice/linkedList/circularLinkedList/IntegerCircularLinkedListImpl.java
@@ -4,19 +4,29 @@ import practice.linkedList.doublyLinkedLIst.adt.DoublyLinkedList;
 import practice.linkedList.doublyLinkedLIst.node.DoublyNode;
 
 public class IntegerCircularLinkedListImpl implements DoublyLinkedList {
+    DoublyNode head;
+    DoublyNode tail;
+    int size;
+
+    public IntegerCircularLinkedListImpl() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
     }
 
     @Override
     public void addFirst(int data) {
 
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
     }
 
     @Override

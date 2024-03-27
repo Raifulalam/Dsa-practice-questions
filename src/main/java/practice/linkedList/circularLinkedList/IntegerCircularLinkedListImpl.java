aeb6bfd86067b8d98a4190965744b5f948222527
Raifulalam <raifulalam0123@gmail.com>
2024-03-28 01:38:34 +0530

    methods of circular linked list is implemented

---
+++ src/main/java/practice/linkedList/circularLinkedList/IntegerCircularLinkedListImpl.java
@@ -0,0 +1,46 @@
+package practice.linkedList.circularLinkedList;
+
+import practice.linkedList.doublyLinkedLIst.adt.DoublyLinkedList;
+import practice.linkedList.doublyLinkedLIst.node.DoublyNode;
+
+public class IntegerCircularLinkedListImpl implements DoublyLinkedList {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void addFirst(int data) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public DoublyNode search(int data) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void add(int data) {
+
+    }
+
+    @Override
+    public void remove(int data) {
+
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+}

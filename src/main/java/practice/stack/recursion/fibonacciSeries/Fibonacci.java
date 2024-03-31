b77fb9996eeedbe4704d074c9c5c49e2ec5f58ad
Raifulalam <raifulalam0123@gmail.com>
2024-04-01 00:24:02 +0530

    recursive fibonacciSeriesFinder is added

--- src/main/java/practice/stack/recursion/fibonacciSeries/Fibonacci.java
+++ src/main/java/practice/stack/recursion/fibonacciSeries/Fibonacci.java
@@ -1,1 +1,28 @@
-recursive factorialalculator is added
+package practice.stack.recursion.fibonacciSeries;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Fibonacci {
+    int fibonacciSeries(int number){
+        if (number == 0 || number==1) {
+            return number;
+        }
+        return fibonacciSeries(number-1)+fibonacciSeries(number-2);
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci=new Fibonacci();
+        System.out.println("fibonacci.fibonacciSeries(10) = " + fibonacci.fibonacciSeries(10));
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci.fibonacciSeries(i)+"  ");
+            array[i]=fibonacci.fibonacciSeries(i);
+        }
+        System.out.println(" ");
+        System.out.println(Arrays.toString(array));
+    }
+}

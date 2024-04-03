0e86004796e61e8b95d00832737d414a8af48e01
Raifulalam <raifulalam0123@gmail.com>
2024-04-04 01:52:06 +0530

    DoublyCircularQueue methods are added

---
+++ src/main/java/practice/queue/queue/doubly/DoublyQueue.java
@@ -0,0 +1,51 @@
+package practice.queue.queue.doubly;
+
+public class DoublyQueue {
+	public DoublyQueueNode front;
+	public DoublyQueueNode rear;
+	int size;
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void enqueue(int data) {
+		DoublyQueueNode node = new DoublyQueueNode(data);
+		if (isEmpty()) {
+			front = rear = node;
+		} else {
+			rear.next = node;
+			node.previous = rear;
+			rear = node;
+		}
+		size++;
+	}
+	
+	public int dequeue() {
+		if (isEmpty()) {
+			System.err.println("Queue is empty!");
+			return Integer.MIN_VALUE;
+		}
+		int removed = front.data;
+		if (front == rear) {
+			front = rear = null;
+			return removed;
+		}
+		front = front.next;
+		front.previous = null;
+		size--;
+		return removed;
+	}
+	
+	public int peek() {
+		if (isEmpty()) {
+			System.err.println("Queue is empty!");
+			return Integer.MIN_VALUE;
+		}
+		return front.data;
+	}
+}
---
+++ src/main/java/practice/queue/queue/doubly/DoublyQueueNode.java
@@ -0,0 +1,16 @@
+package practice.queue.queue.doubly;
+
+public class DoublyQueueNode {
+	public int data;
+	public DoublyQueueNode next;
+	public DoublyQueueNode previous;
+	
+	public DoublyQueueNode(int data) {
+		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "DoublyQueueNode{" + "data=" + data + ", next=" + next + ", previous=" + previous + '}';
+	}
+}

8af31a3b9aa394cc576715a733f8639565d2de3b
Raifulalam <raifulalam0123@gmail.com>
2024-04-04 01:47:15 +0530

    Circular Queue methods are added

--- src/main/java/practice/queue/queue/Main.java
+++ src/main/java/practice/queue/queue/Main.java
@@ -1,21 +1,38 @@
 package practice.queue.queue;
 
-import practice.queue.queue.singly.IntegerQueue;
+import practice.queue.queue.array.IntegerArrayQueue;
 
 public class Main {
     public static void main(String[] args) {
-        IntegerQueue integerQueue = new IntegerQueue();
-        integerQueue.offer(25);
-        integerQueue.offer(20);
-        integerQueue.remove();
-        integerQueue.offer(50);
-        integerQueue.offer(5);
-        integerQueue.offer(2);
-        integerQueue.offer(35);
-        integerQueue.offer(25);
-        integerQueue.offer(27);
-        integerQueue.offer(55);
-        integerQueue.offer(95);
+//        IntegerQueue integerQueue = new IntegerQueue();
+//        integerQueue.offer(25);
+//        integerQueue.offer(20);
+//        integerQueue.remove();
+//        integerQueue.offer(50);
+//        integerQueue.offer(5);
+//        integerQueue.offer(2);
+//        integerQueue.offer(35);
+//        integerQueue.offer(25);
+//        integerQueue.offer(27);
+//        integerQueue.offer(55);
+//        integerQueue.offer(95);
+        IntegerArrayQueue integerArrayQueue = new IntegerArrayQueue(8);
+        integerArrayQueue.enqueue(250);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(45);
+        integerArrayQueue.enqueue(78);
+        integerArrayQueue.dequeue();
+
 
     }
 }
---
+++ src/main/java/practice/queue/queue/circular/CircularQueue.java
@@ -0,0 +1,75 @@
+package practice.queue.queue.circular;
+
+import practice.queue.adt.Queue;
+import practice.queue.node.QueueNode;
+
+public class CircularQueue implements Queue {
+    QueueNode front;
+    QueueNode rear;
+    int size;
+    int capacity;
+
+    public CircularQueue(int capacity) {
+        if (capacity <= 0) {
+            System.err.println("Capacity must be positive!");
+        }
+        this.capacity = capacity;
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    @Override
+    public void offer(int data) {
+        if (isFull()) {
+            System.err.println("Queue is full!");
+            return;
+        }
+        QueueNode node = new QueueNode(data);
+        if (isEmpty()) {
+            front = rear = node;
+            node.next = node;
+        } else {
+            rear.next = node;
+            rear = node;
+            rear.next = front;
+        }
+        size++;
+    }
+
+    @Override
+    public int remove() {
+        if (isEmpty()) {
+            System.err.println("Queue is empty!");
+            return Integer.MIN_VALUE;
+        }
+        int removed = front.data;
+        if (front == rear) {
+            front = rear = null;
+        } else {
+            front = front.next;
+            rear.next = front;
+        }
+        size--;
+        return removed;
+    }
+
+    @Override
+    public int peek() {
+        if (isEmpty()) {
+            System.err.println("Queue is empty!");
+            return Integer.MIN_VALUE;
+        }
+        return front.data;
+    }
+}

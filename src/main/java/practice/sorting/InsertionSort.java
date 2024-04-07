6553638bb82db4fe3b021882cd9efd86b1b46a5d
Raifulalam <raifulalam0123@gmail.com>
2024-04-07 12:57:26 +0530

    InsertionSort method is completed

--- src/main/java/practice/sorting/InsertionSort.java
+++ src/main/java/practice/sorting/InsertionSort.java
@@ -1,6 +1,17 @@
 package practice.sorting;
 
 public class InsertionSort {
+    void sorter(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            int currentElement=array[i];
+            int comparingIndex=i-1;
+            while (comparingIndex>= 0 && array[comparingIndex] > currentElement){
+                array[comparingIndex+1]=array[comparingIndex];
+                comparingIndex--;
+            }
+            array[comparingIndex+1]=currentElement;
+        }
+    }
 
 }
 //arr[]: {12, 11, 13, 5, 6}

b252e547705dd267cd899459c3401f7587f24bb2
Raifulalam <raifulalam0123@gmail.com>
2024-04-09 01:07:06 +0530

    BinaryTree class is implemented

---
+++ src/main/java/practice/tree/binaryTree/BinaryTree.java
@@ -0,0 +1,13 @@
+package practice.tree.binaryTree;
+import practice.tree.TreeNode;
+public class BinaryTree {
+  public TreeNode rootnode;
+
+    public BinaryTree() {
+        this.rootnode = null;
+    }
+
+    public BinaryTree(TreeNode rootnode) {
+        this.rootnode = rootnode;
+    }
+}

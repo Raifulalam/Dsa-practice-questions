343c7ab281efb2c4e44e0532d965a75d533546ce
Raifulalam <raifulalam0123@gmail.com>
2024-04-09 01:26:25 +0530

    insertNode method is added to BinaryTree class

--- src/main/java/practice/tree/binaryTree/BinaryTree.java
+++ src/main/java/practice/tree/binaryTree/BinaryTree.java
@@ -10,4 +10,30 @@ public class BinaryTree {
     public BinaryTree(TreeNode rootnode) {
         this.rootnode = rootnode;
     }
+    public void insertNode(int data){
+        TreeNode node=new TreeNode(data);
+        if (rootnode == null) {
+            this.rootnode=node;
+        }else {
+            TreeNode currentNode=rootnode;
+            TreeNode parentNode ;
+            while (true){
+                parentNode=currentNode;
+                if(currentNode.data>data){
+                    currentNode=currentNode.leftNode;
+                    if (currentNode == null) {
+                        parentNode.leftNode=node;
+                        return;
+                    }
+                } else {
+                    currentNode=currentNode.rightNode;
+                    if (currentNode == null) {
+                        parentNode.rightNode=node;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
 }
